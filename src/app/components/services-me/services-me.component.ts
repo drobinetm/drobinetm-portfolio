3b2169fcb2eebdc6544e27aa1a10592d68a3e1e6
Aliuska Dominguez <adrosales@segurmatica.cu>
2021-04-11 20:47:13 -0400

    Added my services section

--- src/app/components/services-me/services-me.component.ts
+++ src/app/components/services-me/services-me.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {LoadJsonService} from '../../services/load-json/load-json.service';
 
 @Component({
   selector: 'app-services-me',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services-me.component.scss']
 })
 export class ServicesMeComponent implements OnInit {
+  public services: any = undefined;
 
-  constructor() { }
+  constructor(private loadJson: LoadJsonService) {
+    this.services = [];
+   }
 
   ngOnInit(): void {
+    this.loadJson.loadJSON('../../../assets/json/my-services.json').subscribe((json) => {
+      this.services = json[0];
+    });
   }
-
 }
