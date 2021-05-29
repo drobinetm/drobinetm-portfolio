3e9644191bd398e7e1b5d2aaa6be2abce491e67c
Aly <Aly@ALY87>
2021-05-28 22:09:08 -0400

    Added data in resume component.

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {LoadJsonService} from "../../services/load-json/load-json.service";
 
 @Component({
   selector: 'app-resume',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent implements OnInit {
+  public resume = undefined;
+  public summary = undefined;
+  public occupations = undefined;
+  public educations = undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private loadJson: LoadJsonService) {
+    this.resume = this.initResume();
+    this.summary = this.initSummary();
+    this.occupations = this.initOccupation();
+    this.educations = this.initEducation();
   }
 
+  ngOnInit(): void {
+    this.loadJson.loadJSON('../../../assets/json/resume.json').subscribe((json) => {
+      this.resume = json[0].resume;
+      this.summary = json[0].summary;
+      this.occupations = json[0].occupation;
+      this.educations = json[0].education;
+    });
+  }
+
+  protected initSummary() {
+    return {};
+  }
+
+  protected initResume() {
+    return {};
+  }
+
+  protected initOccupation() {
+    return [];
+  }
+
+  protected initEducation() {
+    return [];
+  }
 }
