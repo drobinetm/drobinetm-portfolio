25a6f23f6f2a2612c482043042a7dad99e3babd3
Diovi Robinet Morales <diovi@dspot.com.pl>
2024-11-02 13:14:20 +0300

    :lipstick: [STYLE] Fix the footer component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {LoadJsonService} from '../../services/load-json/load-json.service';
+import {Personal} from '../../models/personal';
+import {Social} from '../../models/social';
 
 @Component({
   selector: 'app-footer',
@@ -6,12 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  name = 'Diovi Robinet Morales';
-  description = 'I\'m a Full Stack Developer ';
+  public personal: Personal = undefined;
+  public social: Social = undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private loadJson: LoadJsonService) {
+    this.personal = this.initPersonal();
+    this.social = this.initSocial();
   }
 
+  ngOnInit(): void {
+    this.loadJson.loadJSON('./assets/json/about-me.json').subscribe((json) => {
+      this.personal = json[0].personal;
+      this.social = json[0].social;
+    });
+  }
+
+  protected initPersonal(): Personal {
+    return {name: '', website: '', phone: '', city: '', age: 0, degree: '', email: '', slogan: '', cvUrl: ''};
+  }
+
+  protected initSocial(): Social {
+    return {github: '', linkedin: '', twitter: '', instagram: ''};
+  }
 }
